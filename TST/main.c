3b65b4036d495d3010c931803e756a2db1a4c28b
Zackory281 <zijincui281@gmail.com>
2017-10-17 17:52:33 -0700

    Initial Commit

---
+++ TST/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TST
+//
+//  Created by Zackory Cramer on 10/17/17.
+//  Copyright © 2017 Zackory Cramer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
